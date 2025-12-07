e9f1145cd2e267a1fdbf4bdc179e2aa343fa31bb
AlexMerchant44 <Alex.merchant23@imperial.ac.uk>
2025-12-07 15:10:49 +0000

    V2

--- camera.py
+++ camera.py
@@ -1,4 +1,3 @@
-# camera.py
 from picamera2 import Picamera2
 import cv2
 
@@ -7,12 +6,18 @@ picam2 = None
 def init_camera():
     global picam2
     picam2 = Picamera2()
-    picam2.configure(picam2.create_still_configuration())
+    config = picam2.create_preview_configuration(
+        main={"size": (1080, 1920)},
+    )
+    picam2.configure(config)
     picam2.start()
+
+init_camera()
 
 def get_frame():
     """Return a BGR frame (numpy array) from the camera."""
     frame = picam2.capture_array()
     # Picamera2 gives RGB; OpenCV uses BGR
     frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    return frame_bgr
+    rotated = cv2.rotate(frame_bgr, cv2.ROTATE_90_CLOCKWISE)
+    return rotated
