2b825e2d489f72d7d5628a2abf3425fb7d6bbb1a
AlexMerchant44 <Alex.merchant23@imperial.ac.uk>
2025-12-19 01:53:28 +0000

    Updated working times

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from src.env.reward import compute_reward
 from src.actuation.action import init_motor, run_motor, cleanup_motor
 
 
-start_time = time(0, 0)   # 7:00
-end_time   = time(7, 0)  # 17:00
+start_time = time(5, 0)   # 7:00
+end_time   = time(17, 0)  # 17:00
 
 
 def is_now_between(start: time, end: time) -> bool:
